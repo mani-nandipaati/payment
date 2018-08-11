eac41ed1d584793ea98350088ea540becb0c8dcc
mani <nandipaati.mani@gmail.com>
2018-08-11 23:43:29 +0530

    initial commit

---
+++ src/main/java/com/cts/cloudpoc/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.cts.cloudpoc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+@ComponentScan(basePackageClasses = PaymentController.class)
+@PropertySource("classpath:swagger.properties")
+public class SwaggerConfig {
+
+	private static final String SWAGGER_API_VERSION = "1.0";
+	private static final String LICENSE_TEXT = "License";
+	private static final String TITLE = "Payment REST API";
+	private static final String DESCRIPTION = " RESTful API for Payments";
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title(TITLE)
+				.description(DESCRIPTION)
+				.license(LICENSE_TEXT)
+				.version(SWAGGER_API_VERSION)
+				.build();
+	}
+	
+	@Bean
+	public Docket paymentsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.paths(PathSelectors.regex("/payment.*"))
+				.build();
+	}
+}
